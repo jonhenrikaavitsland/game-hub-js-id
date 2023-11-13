fc80b2e1d56fb6c7175a4bca053a807f02a2a295
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-11-13 08:43:39 +0100

    test

--- js/index.js
+++ js/index.js
@@ -5,9 +5,14 @@ import {
   openMenu,
   closeMenu,
   closeMenuEsc,
+  menuAnchor,
 } from "./hamburgerMenu/menu.js";
 
 menuIcon.addEventListener("click", openMenu);
 menuCloseIcon.addEventListener("click", closeMenu);
 document.addEventListener("keydown", closeMenuEsc);
 overlay.addEventListener("click", closeMenu);
+
+// for (let i = 0; i < menuAnchor.length; i++) {
+//   menuAnchor[i].addEventListener("click", closeMenu);
+// }
