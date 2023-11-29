da964f55c43c6d23807b5364d3ecc5314a00d030
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2023-11-29 23:48:31 +0100

    fix

--- js/render/renderTitle.js
+++ js/render/renderTitle.js
@@ -3,18 +3,14 @@ import { titleImage } from "./titleImage.js";
 import { purchaseContainer } from "./purchaseContainer.js";
 
 export function renderTitle(game, parent, title, data) {
-  // Location to append.
-  const parentContainer = parent;
-  console.log(data);
-
   // Sets the title tag in head section.
   const titleTag = title;
-  titleTag.textContent = `Game Hub | ${game.title}`;
+  titleTag.textContent = "Game Hub | " + game.title;
 
   // First Section Start
-  titleSection(game, parentContainer, data);
-  titleImage(game, parentContainer);
-  purchaseContainer(game, parentContainer);
+  titleSection(game, parent, data);
+  titleImage(game, parent);
+  purchaseContainer(game, parent);
 
   // Product description
 }
